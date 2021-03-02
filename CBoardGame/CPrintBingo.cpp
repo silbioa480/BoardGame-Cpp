ad151c630e846b25e03cc75f7976b2d785bab68f
silbioa480 <silbioa480@gmail.com>
2021-03-02 20:13:57 +0900

    modified CPrintBingo.cpp

--- CBoardGame/CPrintBingo.cpp
+++ CBoardGame/CPrintBingo.cpp
@@ -7,21 +7,19 @@ void CPrintBingo::printUITop()
 
 void CPrintBingo::printLine(int si, int _r)
 {
-	string s = m_sLINE[si];
+	int t = 0;
+	while (t != 2)
+	{
+		string s = m_sLINE[si];
+		cout << getLine(s, _r, 0);
+		s = m_sLINE[0] + m_sLINE[si + 1];
+		for (int i = 1; i <= (getColSize() / 2) - 2; i++) cout << getLine(s, _r, i);
+		s = m_sLINE[0] + m_sLINE[si + 2];
+		cout << getLine(s, _r, getColSize() - 1);
 
-	cout << getLine(s, _r, 0);
-	s = m_sLINE[0] + m_sLINE[si + 1];
-	for (int i = 1; i <= (getColSize() / 2) - 2; i++) cout << getLine(s, _r, i);
-	s = m_sLINE[0] + m_sLINE[si + 2];
-	cout << getLine(s, _r, getColSize() - 1) << " ";
-
-	s = m_sLINE[si];
-	cout << getLine(s, _r, 0);
-	s = m_sLINE[0] + m_sLINE[si + 1];
-	for (int i = 1; i <= (getColSize() / 2) - 2; i++) cout << getLine(s, _r, i);
-	s = m_sLINE[0] + m_sLINE[si + 2];
-	cout << getLine(s, _r, getColSize() - 1);
-
+		if (t == 0) printf(" ");
+		t++;
+	}
 	printUIRight(_r);
 }
 
@@ -31,21 +29,14 @@ void CPrintBingo::printLineWide(int _r)
 	cout << s;
 
 	int data;
-	for (int _c = 0; _c < (getColSize() / 2) - 1; _c++)
+	for (int _c = 0; _c < getColSize() - 2; _c++)
 	{
 		data = getData(_r, _c);
 		if (data == -1) cout << m_sSTONE[2];
 		else printf("%02d", getData(_r, _c));
 		cout << s;
-	}
-	cout << " " << s;
 
-	for (int _c = 5; _c < getColSize() - 2; _c++)
-	{
-		data = getData(_r, _c);
-		if (data == -1) cout << m_sSTONE[2];
-		else printf("%02d", getData(_r, _c));
-		cout << s;
+		if (_c == 4) cout << " " << s;;
 	}
 	cout << "\n";
 }
